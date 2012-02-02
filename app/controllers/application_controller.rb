1d6bbab585e93518c8866d1c2bbf0d4c4fd4d779
Philip Daineko <daineko.f@gmail.com>
2012-02-03 01:20:46 +0300

    login popup, i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,42 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_subdomain || I18n.default_locale
+  end
+
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.empty? ? 'pirate' : request.subdomains.first
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale : nil
+  end
+
   private
 
   #there are many paths available e.g. after_sign_up_path_for, after_sign_out_path_for etc
-  def after_sign_in_path_for(resource_or_scope)
-    #crappy code because of L:13
-    #if sign up then to user#show else user#edit
-    current_user.sign_in_count <= 1 ? new_user_path : current_user
+  #def after_sign_in_path_for(resource_or_scope)
+  #  #crappy code because of L:13
+  #  #if sign up then to user#show else user#edit
+  #  current_user.sign_in_count <= 1 ? new_user_path : current_user
+  #end
+  #
+  ##doesn't work!!!
+  #def after_sign_up_path_for(resource_or_scope)
+  #  new_user_path
+  #end
+
+  #code to allow login from wherever
+  def resource_name
+    :user
   end
 
-  #doesn't work!!!
-  def after_sign_up_path_for(resource_or_scope)
-      new_user_path
-    end
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
