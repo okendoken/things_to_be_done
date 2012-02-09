f7e5429bcec687162db3e3c777fcfe37af479ae1
Philip Daineko <daineko.f@gmail.com>
2012-02-10 01:20:50 +0300

    project and task page

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,11 @@ class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
+  has_many :participations
   has_many :votes, :as => :target
+  has_many :users, :through => :votes
+
+  has_many :participants, :through => :participations, :source => :user
 
   include VoteTarget
 
