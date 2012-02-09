d3824e76f2a80f73bd4c42cbfa55fa40d96d0d83
Philip Daineko <daineko.f@gmail.com>
2012-02-10 00:09:35 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,19 @@
 u = User.create!(:email => 'test@example.com', :password => 'test123')
-p = Project.create!(:user => u, :title => 'Make world better', :description => "We have to move world forward by making it better
-and solving problems", :problem => "If we won't make world better it'll be getting worse")
-t1 = Task.create!(:project => p, :user => u, :title => 'Complete TTBD', :description => "We have six reasons why we have to complete TTBD:\n
-1. we'll feel better. we'll be able to say 'we've built company!'\n2. we'll have money\n3. we'll make world better\n
-4. feeling of justice is very strong among us. there are so many unjustice around.\n5*.TTBD is really focused.
-We see it future. The future is TTBD. \n6*. Paul Graham said he is going to invest in such kind of projects")
-t2 = Task.create!(:project => p, :user => u, :title => 'Promote TTBD', :description => "Seems that TTBD will become popular by its own,
+problem = Problem.create!(:user => u, :title => 'There are many problems in the World',
+                          :description => "If we won't make world better it'll be getting worse")
+p = Project.create!(:user => u, :title => 'Make world better',
+                    :description => "We have to move world forward by making it better and solving problems",
+                    :problem => problem)
+t1 = Task.create!(:project => p, :user => u, :title => 'Complete TTBD',
+                  :description => "We have six reasons why we have to complete TTBD:\n
+                  1. we'll feel better. we'll be able to say 'we've built company!'\n
+                  2. we'll have money\n
+                  3. we'll make world better\n
+                  4. feeling of justice is very strong among us. there are so many unjustice around.\n
+                  5*.TTBD is really focused. We see it future. The future is TTBD.\n
+                  6*. Paul Graham said he is going to invest in such kind of projects")
+t2 = Task.create!(:project => p, :user => u, :title => 'Promote TTBD',
+                  :description => "Seems that TTBD will become popular by its own,
 but we have to put efforts to make it happen")
 
 t1.vote_for_this(u)
