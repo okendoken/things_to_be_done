5ac182e59860d969cda0b567f36a3ef0eef4bccd
Philip Daineko <daineko.f@gmail.com>
2012-02-15 01:14:02 +0300

    ajax vote functionality is done

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,5 +1,4 @@
 class VoteController < ApplicationController
-  before_filter :authenticate_user!
 
   def support
     handle_request(true)
@@ -12,38 +11,44 @@ class VoteController < ApplicationController
   private
 
   def handle_request(positive)
-    if params[:task_id].present?
-      task = Task.find(params[:task_id])
-      vote_for_task(task, positive)
-    elsif params[:id].present?
-      project = Project.find(params[:id])
-      vote_for_project(project, positive)
+    if user_signed_in?
+      if params[:task_id].present?
+        task = Task.find(params[:task_id])
+        vote_for_task(task, positive)
+      elsif params[:id].present?
+        project = Project.find(params[:id])
+        vote_for_project(project, positive)
+      end
+      render 'vote/support'
+    else
+      render 'vote/sign_in'
     end
-    render 'vote/support'
   end
 
   def vote_for_project(project, positive = true)
-    if vote = project.votes.where(:user_id => current_user.id)[0]
+    if vote = project.user_vote(current_user)
       if positive ^ vote.positive?
         vote.positive = positive
         vote.save
       end
     else
-      project.vote_for_this(current_user, positive)
+      vote = project.vote_for_this(current_user, positive)
     end
     #todo someday we can rerender only current user instead of fetching all
     @supporters = project.users
+    @supports = vote.positive?
   end
 
   def vote_for_task(task, positive = true)
-    if vote = task.votes.where(:user_id => current_user.id)[0]
+    if vote = task.user_vote(current_user)
       if positive ^ vote.positive?
         vote.positive = positive
         vote.save
       end
     else
-      task.vote_for_this(current_user, positive)
+      vote = task.vote_for_this(current_user, positive)
     end
     @supporters = task.users
+    @supports = vote.positive?
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
-//= require autocomplete-rails
-
 function popupCenter(url, width, height, name) {
-  var left = (screen.width/2)-(width/2);
-  var top = (screen.height/2)-(height/2);
-  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
+    return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
 }
+
+function changeLikerState(supports){
+    if (supports){
+        $("#supp-link").addClass("disabled");
+        $("#dont-supp-link").removeClass("disabled");
+    } else {
+        $("#supp-link").removeClass("disabled") ;
+        $("#dont-supp-link").addClass("disabled");
+    }
+}
+
+$(function(){
+    //if disabled class present then we don't need to let this btn or link being executed
+    $("body").delegate("[class*='disabled']", "click", function(event){
+    	return false;
+    });
+});
