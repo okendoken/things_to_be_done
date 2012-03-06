d1883cef7c5201880a5befa08f54af867adfdb5d
Philip Daineko <daineko.f@gmail.com>
2012-03-06 17:03:16 +0300

    prepare for YC

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,7 @@ t1 = Task.create!(:project => p, :user => u, :title => 'Complete TTBD',
                   2. we'll have money\n
                   3. we'll make world better\n
                   4. feeling of justice is very strong among us. there are so many unjustice around.\n
-                  5*. TTBD is really focused. We see it future. The future is TTBD.\n
-                  6*. Paul Graham said he is going to invest in such kind of projects")
+                  5*. TTBD is really focused. We see it future. The future is TTBD.")
 t2 = Task.create!(:project => p, :user => u, :title => 'Promote TTBD',
                   :description => "Seems that TTBD will become popular by its own,
 but we have to put efforts to make it happen")
