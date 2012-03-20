e311358b667a243d8fe0a48a844adac25a4e3062
randrianov <r0mkin@ya.ru>
2012-03-21 01:49:54 +0300

    philip sorry bad merging and proebal your CanCan activity

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,7 @@ class Ability
       #todo user creator of project/task can delete comments? think about it
       comment.user == user or comment.target.user == user
     end
-    can :destroy, RelatedEvent do |event|
+    can :manage, RelatedEvent do |event|
       event.reader == user
     end
     can :manage, [Project, Task], :user_id => user.id
