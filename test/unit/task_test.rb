935e5a3fd4ef7c8dfa550677e44aedac870b2757
Philip Daineko <daineko.f@gmail.com>
2012-05-13 11:35:17 +0300

    tests after BSUIR

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save task if project_id is set" do
+    u = User.create!(:email => 'test5@example.com', :password => 'test123', :nickname => 'First User')
+    t = Task.new(:title => 'Promote TTBD',
+                 :description => "Seems that TTBD will become popular by its own, but we have to put efforts to make it happen")
+    assert t.save
+  end
 end
